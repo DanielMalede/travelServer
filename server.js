8541e0453df95f7ea6ec7ceb3939049c4055dfd1
DanielMalede <danielmalede@gmail.com>
2022-11-13 21:34:25 +0200

    .

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const travelRouter = require('./routes/travel-router')
-const companysCompanysRouter = require('./routes/flightCompanys-router')
-const flightsRouter = require('./routes/flight-router')
+const travelRouter = require("./routes/travel-router");
+const companysCompanysRouter = require("./routes/flightCompanys-router");
+const flightsRouter = require("./routes/flight-router");
 const app = express();
 const port = 4500;
 
@@ -10,9 +10,9 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/travel',travelRouter)
-app.use('/flightCompanys',companysCompanysRouter)
-app.use('/flights',flightsRouter)
+app.use("/travel", travelRouter);
+app.use("/flightCompanys", companysCompanysRouter);
+app.use("/flights", flightsRouter);
 
 app.get("/", (req, res) => {
   res.send("success");
