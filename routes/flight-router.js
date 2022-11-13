a62aad0218ec9ca08f90327e335d23de0d21e532
DanielMalede <danielmalede@gmail.com>
2022-11-13 21:34:44 +0200

    .

--- routes/flight-router.js
+++ routes/flight-router.js
@@ -1,23 +1,26 @@
-const router = require('express').Router()
+const router = require("express").Router();
 const {
-    getFlights,
-    getFlightById,
-    createFlight,
-    updateFlights,
-    deleteFlight,
-    getFlightByNumFlight,
-    getAllFlightsFirstClass,
-    getFlightByTimeDepartureTime
-} = require('../controllers/flights-ctrl')
+  getFlights,
+  getFlightById,
+  createFlight,
+  updateFlights,
+  deleteFlight,
+  getFlightByNumFlight,
+  getAllFlightsFirstClass,
+  getFlightByTimeDepartureTime,
+} = require("../controllers/flights-ctrl");
 
-router.get('/',getFlights)
-router.get('/getById/:id',getFlightById)
-router.post('/addFlight',createFlight)
-router.put('/updateFlight/:id',updateFlights)
-router.delete('/delete/:id',deleteFlight)
-router.get('/getFlightByNun/:flightNum',getFlightByNumFlight)
-router.get('/getFirstClassFlights',getAllFlightsFirstClass)
-router.get('/flightsByTimeDeparture/:departureTime',getFlightByTimeDepartureTime)
-router.get('/searchFlightsByLandingAndDeparture',getFlightByTimeDepartureTime)
+router.get("/", getFlights);
+router.get("/getById/:id", getFlightById);
+router.post("/addFlight", createFlight);
+router.put("/updateFlight/:id", updateFlights);
+router.delete("/delete/:id", deleteFlight);
+router.get("/getFlightByNun/:flightNum", getFlightByNumFlight);
+router.get("/getFirstClassFlights", getAllFlightsFirstClass);
+router.get(
+  "/flightsByTimeDeparture/:departureTime",
+  getFlightByTimeDepartureTime
+);
+router.get("/searchFlightsByLandingAndDeparture", getFlightByTimeDepartureTime);
 
-module.exports = router
+module.exports = router;
