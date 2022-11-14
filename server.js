e4880120b18d015ba5392de3d3c1f4098e2fbbfe
DanielMalede <danielmalede@gmail.com>
2022-11-14 18:27:57 +0200

    .

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/travel", travelRouter);
 app.use("/flightCompanys", companysCompanysRouter);
 app.use("/flights", flightsRouter);
-
 app.get("/", (req, res) => {
   res.send("success");
 });
