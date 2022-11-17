b701164149a9e71ae3e6d0d57027d99765b0c164
DanielMalede <danielmalede@gmail.com>
2022-11-17 15:42:16 +0200

    .

--- routes/travel-router.js
+++ routes/travel-router.js
@@ -1,16 +1,16 @@
-const router  = require('express').Router()
+const router = require("express").Router();
 const {
-    getTravel,
-    createTravelById,
-    getTravelById,
-    deleteTravel,
-    updateTravel
-} = require('../controllers/travel-ctrl')
+  getTravel,
+  createTravelById,
+  getTravelById,
+  deleteTravel,
+  updateTravel,
+} = require("../controllers/travel-ctrl");
 
-router.get("/",getTravel)
-router.get('/getById/:id',getTravelById)
-router.post('/saveData',createTravelById)
-router.delete('/delete/:id',deleteTravel)
-router.put('/update/:id',updateTravel)
+router.get("/", getTravel);
+router.get("/getById/:id", getTravelById);
+router.post("/saveData", createTravelById);
+router.delete("/delete/:id", deleteTravel);
+router.put("/update/:id", updateTravel);
 
-module.exports = router
+module.exports = router;
