9cc6fcc4196fe6bda22d905a61ef60280d46e9b8
DanielMalede <danielmalede@gmail.com>
2022-11-17 15:16:22 +0200

    .

--- controllers/users-ctrl.js
+++ controllers/users-ctrl.js
@@ -1,20 +1,20 @@
-const users = require('../model/registerUsers-model')
-const {signUpValidator }= require('../validation/validateEmail')
+const users = require("../model/registerUsers-model");
+const { signUpValidator } = require("../validation/validateEmail");
 
-const signUp = (req,res)=>{
-    if (!signUpValidator(req.body.users.email)) {
-        return res.status(400).json('err')
-    }
-    users.push(req.body.users)
-}
-const signIn = (req,res)=>{
-    if (validationEmail(req.body.users.email)){
-        return res.send('your in')
-    }
-    return res.send('you not in')
-}
+const signUp = (req, res) => {
+  if (!signUpValidator(req.body.users.email)) {
+    return res.status(400).json("err");
+  }
+  users.push(req.body.users);
+};
+const signIn = (req, res) => {
+  if (validationEmail(req.body.users.email)) {
+    return res.send("your in");
+  }
+  return res.send("you not in");
+};
 
 module.exports = {
-    signUp,
-    signIn
-}
+  signUp,
+  signIn,
+};
