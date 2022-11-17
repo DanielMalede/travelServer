17ea8e1cb14911c876c5f7b62d2031bad0d11646
DanielMalede <danielmalede@gmail.com>
2022-11-17 15:42:30 +0200

    ..

--- routes/flightCompanys-router.js
+++ routes/flightCompanys-router.js
@@ -1,20 +1,20 @@
-const router = require('express').Router()
+const router = require("express").Router();
 const {
-    getFlightcompanys,
-    createFlightcompanys,
-    getFlightcompanysById,
-    deleteFlightcompanys,
-    updateFlightcompanys,
-    getFlightByName,
-    getSingleFlightByName
-} =require('../controllers/flightCompanys-ctrl')
+  getFlightcompanys,
+  createFlightcompanys,
+  getFlightcompanysById,
+  deleteFlightcompanys,
+  updateFlightcompanys,
+  getFlightByName,
+  getSingleFlightByName,
+} = require("../controllers/flightCompanys-ctrl");
 
-router.get('/',getFlightcompanys)
-router.post('/savaData',createFlightcompanys)
-router.get('/getById/:id',getFlightcompanysById)
-router.delete('/delete/:id',deleteFlightcompanys)
-router.put('/update/:id',updateFlightcompanys)
-router.get('/getByName/:id',getFlightByName)
-router.get('/getSingleFlightByName/:id',getSingleFlightByName)
+router.get("/", getFlightcompanys);
+router.post("/savaData", createFlightcompanys);
+router.get("/getById/:id", getFlightcompanysById);
+router.delete("/delete/:id", deleteFlightcompanys);
+router.put("/update/:id", updateFlightcompanys);
+router.get("/getByName/:id", getFlightByName);
+router.get("/getSingleFlightByName/:id", getSingleFlightByName);
 
-module.exports = router
+module.exports = router;
