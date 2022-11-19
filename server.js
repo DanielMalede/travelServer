06c9874da6ed4496e6225640d1c2ac6930cb868e
DanielMalede <danielmalede@gmail.com>
2022-11-19 17:41:45 +0200

    .

--- server.js
+++ server.js
@@ -32,8 +32,7 @@ const middleWareFlightCompanyAccesses = (req, res, next) => {
     : res.send("email not find");
 };
 
-app.use("/flightCompanys",middleWareFlightCompanyAccesses,
-  companysCompanysRouter
+app.use("/flightCompanys",middleWareFlightCompanyAccesses,companysCompanysRouter
 );
 app.use("/travel", travelRouter);
 app.use("/flights", flightsRouter);
