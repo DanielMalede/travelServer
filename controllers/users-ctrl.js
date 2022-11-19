6619dbf42428458ecd15348c044ce3071ec12c52
DanielMalede <danielmalede@gmail.com>
2022-11-19 16:41:06 +0200

    .

--- controllers/users-ctrl.js
+++ controllers/users-ctrl.js
@@ -1,26 +1,27 @@
 const users = require("../model/registerUsers-model");
 const { emailValidator } = require("../validation/validateEmail");
 const { validationPassword } = require("../validation/validationPassword");
-const { validationPassword } = require("../validation/validationPassword");
+const checkUserAge = require("../validation/validateUserAge");
 
-const getUsers = (req,res)=>{
-  res.send(users)
-}
+const getUsers = (req, res) => {
+  res.send(users);
+};
 const signUp = (req, res) => {
   if (
-    emailValidator(res,req.body.users.email) &&
+    emailValidator(res, req.body.users.email) &&
     validationPassword(
       res,
       req.body.users.password,
       req.body.users.validatorPassword
-    )
+    ) &&
+    checkUserAge(req.body.users.age)
   ) {
-    return (res.send("user has created"),users.push(req.body.users));
+    return res.send("user has created"), users.push(req.body.users);
   }
   return res.send("error");
 };
 
 module.exports = {
   signUp,
-  getUsers
+  getUsers,
 };
--- validation/validateUserAge.js
+++ validation/validateUserAge.js
@@ -1,5 +1,8 @@
-const checkUserAge = ()=>{
-
+const checkUserAge = (age)=>{
+    if(age>18){
+        return true
+    }
+    return false
 }
 
 module.exports = checkUserAge
