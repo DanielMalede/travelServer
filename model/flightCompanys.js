f31e9a0c43fef40212ed44022b6aa46386f97fca
DanielMalede <danielmalede@gmail.com>
2022-11-23 11:18:27 +0200

    .

--- model/flightCompanys.js
+++ model/flightCompanys.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const FlightCompany = new Schema({
-  id:{type:Number},
-  name:{type:String},
-  airplane:Number,
-  numAirPlaneAvailable:Number
-})
+  id: { type: Number },
+  name: { type: String },
+  airplane: Number,
+  numAirPlaneAvailable: Number,
+});
 
-module.exports = mongoose.model('flightCompany',FlightCompany);
+module.exports = mongoose.model("flightCompany", FlightCompany);
