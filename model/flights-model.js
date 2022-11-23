b8deb2f55544dcf94019a0d1ac6f52db85880d44
DanielMalede <danielmalede@gmail.com>
2022-11-23 11:18:12 +0200

    .

--- model/flights-model.js
+++ model/flights-model.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const Flights = new Schema({
-  id:{type:Number,required:true},
-  flightNum:{type:Number,required:true},
-  departureFlight:{type:String,required:true},
-  flightLanding:{type:String,required:true},
-  seatNum:{type:Number,required:true},
-  departureTime:{type:String,required:true},
-  LandingTime:{type:String,required:true},
-  firstClass:Boolean,
-  seatsAvailable:{type:Number,required:true}
-})
+  id: { type: Number, required: true },
+  flightNum: { type: Number, required: true },
+  departureFlight: { type: String, required: true },
+  flightLanding: { type: String, required: true },
+  seatNum: { type: Number, required: true },
+  departureTime: { type: String, required: true },
+  LandingTime: { type: String, required: true },
+  firstClass: Boolean,
+  seatsAvailable: { type: Number, required: true },
+});
 
-
-module.exports = mongoose.model('flight',Flights);
+module.exports = mongoose.model("flight", Flights);
