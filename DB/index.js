1ca08411a6c64c57c408db7245c06a4de0894213
DanielMalede <danielmalede@gmail.com>
2022-11-29 11:18:58 +0200

    .

--- DB/index.js
+++ DB/index.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 const CONNECTION_STRING = process.env.CONNECTION_STRING;
 mongoose
-  .connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("connection succeed"))
-  .catch((error) => {
-    console.log("connection ", error.message);
-  });
+  .catch((error) => console.log("connection ", error.message));
 
 module.exports = mongoose.connection;
